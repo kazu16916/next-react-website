f1b6776f19c9780f8e407910b4d15756b2eb2b13
kazu16916 <130880184+kazu16916@users.noreply.github.com>
2023-12-15 16:28:32 +0900

    1215_7.5-8.4

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,9 +1,38 @@
-import { client } from 'lib/api'
-export default function Schedule(){
-    return <h1>記事タイトル</h1>
+import { client, getPostBySlug } from 'lib/api'
+import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import Image from 'next/image'
+export default function Schedule({
+    title,
+    publish,
+    content,
+    eyecatch,
+    categories,
+}) {
+    return (
+        <Container>
+            <article>
+                <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+                <figure>
+                    <Image 
+                      src={eyecatch.url}
+                      alt=""
+                      layout="responsive"
+                      width={eyecatch.width}
+                      height={eyecatch.height}
+                      sizes="(min-width:1152px) 1152px,100vw"
+                      priority
+                    />
+                </figure>
+            </article>
+        </Container>
+    )
 }
 
 export async function getStaticProps(){
+    const slug='schedule'
+    const post=await getPostBySlug(slug)
+
     console.log('処理1')
     setTimeout(()=>console.log('処理2'),1000)
     console.log('処理3')
@@ -20,7 +49,13 @@ export async function getStaticProps(){
     resPromise.then((res) => console.log(res)).catch((err)=>console.log(err))
     
     return {
-        props:{},
+        props:{
+            title:post.title,
+            publish:post.publishDate,
+            content:post.content,
+            eyecatch:post.eyecatch,
+            categories:post.categories,
+        },
     }
 }
 
